8967355130ded570b8468375841d8964a0af93bd
Pranav Joshi <pranav.joshi@iitgn.ac.in>
2025-02-12 02:44:09 +0530

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -1,19 +1,31 @@
-from SYNTAX import *
 import argparse
+import SYNTAX
+SYNTAX.DEBUG_WITH_COLOR = True
+SYNTAX.TESTING = False
+from SYNTAX import *
 parser = argparse.ArgumentParser()
 parser.add_argument("file")
 args = parser.parse_args()
 file = args.file
 s = open(file).read()
-print("Program:")
-print("-"*50)
-print(HighLight(s))
-print("-"*50)
+print("_"*50 + "\n")
+print("Program")
+print("_"*50 + "\n")
+if DEBUG_WITH_COLOR:print(HighLight(s))
+else:print(TokensToStr(lex(s,True,True),color =False))
+print("_"*50 + "\n")
 s = lex(s)
 s0 = S0()
-s0.parse(s)
-print("Output:")
-print("-"*50)
-s0.eval()
-print("\n\n" + "-"*50)
-print("Program Finished")
+res = s0.parse(s)
+if isinstance(res,ValueError):
+    if DEBUG_WITH_COLOR:print("\x1b[38;2;255;0;0mCompilation Error\x1b[0m")
+    else:print("Compilation Error")
+    print("_"*50 + "\n")
+    PrintError(res)
+else:
+    if DEBUG_WITH_COLOR:print("\x1b[38;2;0;0;255mOutput\x1b[0m")
+    else:print("Output:")
+    print("_"*50 + "\n")
+    res = s0.eval()
+    if isinstance(res,ValueError):PrintError(res)
+    print("\n\n" + "_"*50 + "\n")
