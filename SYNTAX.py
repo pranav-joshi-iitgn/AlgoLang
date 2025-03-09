d52f1e78d3b60cfb25d37fb4232af164e5829c98
Pranav Joshi <pranav.joshi@iitgn.ac.in>
2025-03-09 22:58:22 +0530

    Add files via upload
    fixed small bug

--- SYNTAX.py
+++ SYNTAX.py
@@ -424,7 +424,7 @@ class List(UnaryOperation):
             "add $t0,$s5,$zero",
             "lw $t1,0($s1) # n",
             "sll $t1,$t1,2 # 4n",
-            "addi $t2,$t1,1 # 4n+4",
+            "addi $t2,$t1,4 # 4n+4",
             "add $s5,$s5,$t2 # inc $s5 by 4n+4",
             "sw $t0,0($s1) # store pointer on stack",
             "sw $t1,0($t0) # store 4n as first thing",
