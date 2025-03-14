592228fd0816291d94de71cc6e39571a0c1adcb7
Pranav Joshi <pranav.joshi@iitgn.ac.in>
2025-03-14 16:06:33 +0530

    Add files via upload

--- SYNTAX.py
+++ SYNTAX.py
@@ -243,24 +243,28 @@ class Id(Token):
             else:
                 labels +=1
                 code.extend([
+                    #
+                    "add $t4,$zero,$t0 # make a copy",
+                    "lw $t0,-12($t0) # fake parent stack",
+                    f"bne $t0,$zero,label{labels} # even the fake parent is dead .. 1 ",
+                    #MIPSPrint("parent dead, checking for fake parent again"),
+                    #
                     "li $t5,0x7F000000",
                     "li $t6,0x1FFFFFFF",
-                    "lw $t1,-8($t0) # self",
+                    "lw $t1,-8($t4) # self",
                     "and $t1,$t1,$t6",
                     "or $t1,$t1,$t5",
                     "lw $t2,0($t1) # parent",
-                    "lw $t0,-4($t0)",
+                    "lw $t0,-4($t4)",
                     "lw $t3,-8($t0) # self_new",
                     f"beq $t3,$t2,label{labels}",
-                    MIPSPrint("parent dead, checking for fake parent"),
-                    "add $t3,$zero,$t0 # make a copy",
-                    "lw $t0,-12($t0) # fake parent stack",
-                    f"bne $t0,$zero,label{labels} # even the fake parent is dead .. 1 ",
+                    #MIPSPrint("parent dead, checking for fake parent"),
+                    
                     "and $t2,$t2,$t6",
                     "or $t2,$t2,$t5",
                     "lw $t0,4($t2) # depend on fake parent",
                     "beq $t0,$zero,error # even the fake parent is dead ... 2",
-                    "sw $t0,-12($t3) # update the fake parent",
+                    "sw $t0,-12($t4) # update the fake parent", #
                     f"label{labels}:# creator alive checking over",
                     ])
         raise RuntimeError(f"variable {x} not found in any scope")
